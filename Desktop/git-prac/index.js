c02d0c1de68f6a56099732111ff595d9cc60af04
Patrick <deguzmanpatrick.cdlb@gmail.com>
2024-02-21 00:21:40 +0800

    First commit this is my first commit

---
+++ Desktop/git-prac/index.js
@@ -0,0 +1,16 @@
+// const fs = require('fs');
+
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+// var superheroes = require("superheroes");
+
+// var mySpName = superheroes.random();
+
+// console.log(mySpName);
+
+const supervillains = require("supervillains");
+var superVillains = require("supervillains");
+
+var spVillains = supervillains.random();
+
+console.log(spVillains);
